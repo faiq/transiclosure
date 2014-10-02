a5e927b7a7e9a18bdc4defb33798eb43c34ccc4f
Matt Robinson <mrobinson7627@gmail.com>
2014-10-01 23:16:48 -0400

    Did some more process stuff

--- processes/wtc_proc.c
+++ processes/wtc_proc.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/shm.h>
+#include <strings.h>
 
 int main(int argc, char** argv) {
     FILE* fp = fopen(argv[1], "r");
@@ -13,6 +14,7 @@ int main(int argc, char** argv) {
     size_t len = 0;
     ssize_t read;
     int p, n;
+    size_t graph_len = n*n;
 
     read = getline(&line, &len, fp);
     p = atoi(line);
@@ -20,9 +22,14 @@ int main(int argc, char** argv) {
     n = atoi(line);
 
     key_t shmkey = ftok("/dev/null", 5);
-    int shmid = shmget(shmkey, n * n + sizeof(int) * p, 0644 | IPC_CREAT);
+    int shmid = shmget(shmkey, graph_len * sizeof(int), 0644 | IPC_CREAT);
+    int* g = (int*)shmat(shmid, NULL, 0);
+    bzero(g, graph_len*sizeof(int));
 
     while ((read = getline(&line, &len, fp)) != -1) {
+        int i = line[0];
+        int j = line[2];
+        g[j*n + i] = 1;
     }
 
     return 0;
