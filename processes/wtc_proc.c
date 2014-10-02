bd3ca6003904aa8ad50fc8fe8bed5f8ecb72c19c
Matt Robinson <mrobinson7627@gmail.com>
2014-10-02 00:04:55 -0400

    This should be done but it segfaults
    It's probably like an off by one error somewhere, so I'm just going to
    go through it and figure it out.

--- processes/wtc_proc.c
+++ processes/wtc_proc.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/shm.h>
 #include <strings.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char** argv) {
     FILE* fp = fopen(argv[1], "r");
@@ -30,6 +32,33 @@ int main(int argc, char** argv) {
         int i = line[0];
         int j = line[2];
         g[j*n + i] = 1;
+    }
+
+    for (int k = 0; k < n; k++) {
+        int work_size = n/p;
+        int extra = n % p;
+        for (int processes = 0; processes < p; processes++) {
+            int work_start = processes * work_size;
+            if (fork() == 0) {
+                for (int i = work_start; i < work_start + work_size; i++) {
+                    for (int j = 0; j < n; j++) {
+                        g[j*n + i] = g[j*n + i] || (g[k*n + i] && g[j*n + k]);
+                    }
+                }
+                exit(EXIT_SUCCESS);
+            }
+        }
+        for (int processes = 0; processes < p; processes++) {
+            wait(0);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (g[j*n + i] == 1) {
+                printf("%d %d\n", i, j);
+            }
+        }
     }
 
     return 0;
